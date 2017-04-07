75bf52273d99f034565e2eb0a9b49cd72870855d
mbuthiya <jamesmuriukimbuthia@gmail.com>
2017-04-07 11:43:14 +0300

    finished the upcoming logic

--- app/src/main/java/com/example/james/moove/MovieFragments/Upcoming.java
+++ app/src/main/java/com/example/james/moove/MovieFragments/Upcoming.java
@@ -1,16 +1,37 @@
 package com.example.james.moove.MovieFragments;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.james.moove.Adapters.TvMoviePageAdapter;
+import com.example.james.moove.HttpService.HttpService;
+import com.example.james.moove.Model.Movie;
+import com.example.james.moove.ProcessResults.ProcessResults;
 import com.example.james.moove.R;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class Upcoming extends Fragment {
+    @Bind(R.id.rv_upcoming)RecyclerView mUpcomingRecycler;
+    private TvMoviePageAdapter tvMoviePageAdapter;
+    private GridLayoutManager gridLayoutManager;
+    public static  final  String UPCOMING="upcoming";
+    private HttpService httpService;
+    private ProcessResults processResults;
+    ArrayList<Movie> movies;
 
 
     public Upcoming() {
@@ -22,7 +43,39 @@ public class Upcoming extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upcoming, container, false);
+        View view=inflater.inflate(R.layout.fragment_upcoming, container, false);
+        ButterKnife.bind(this,view);
+        return view;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        httpService=new HttpService();
+        processResults=new ProcessResults();
+        GetUpcoming upcoming=new GetUpcoming();
+        upcoming.execute(UPCOMING);
+    }
+    class GetUpcoming extends AsyncTask<String,Void,String>{
+        @Override
+        protected String doInBackground(String... params) {
+            String movie=params[0];
+            String result=null;
+            try{
+                result=httpService.getMovieTypes(movie);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            movies=processResults.movieResults(s);
+            tvMoviePageAdapter=new TvMoviePageAdapter(getContext(),movies);
+            mUpcomingRecycler.setAdapter(tvMoviePageAdapter);
+            gridLayoutManager=new GridLayoutManager(getContext(),2);
+            mUpcomingRecycler.setLayoutManager(gridLayoutManager);
+        }
+    }
 }
