7aaad6af00e0c27000a4c0cddbc7c56e96b20588
mbuthiya <jamesmuriukimbuthia@gmail.com>
2017-04-08 09:20:33 +0300

    added api service to retrieve information on tv shows from the api

--- app/src/main/java/com/example/james/moove/Constants/Constants.java
+++ app/src/main/java/com/example/james/moove/Constants/Constants.java
@@ -11,10 +11,14 @@ public class Constants {
     //https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
     public static final String MOVIE_SEARCH_BASE_URL="https://api.themoviedb.org/3/search/movie";
 
-    //add the movie id then the api key ie.https://api.themoviedb.org/3/movie/343611?api_key={api_key}
-    public static final String MOVIE_DETAILS_BASE_URL="https://api.themoviedb.org/3/movie/";
     //now_playing
     public static final String PLAYING_BASE_URL="https://api.themoviedb.org/3/movie/";
 
     public static  final String MOVIE_POSTER_BASE_URL="https://image.tmdb.org/t/p/w500/";
+
+
+    //on_the_air,popular,airing_today
+
+    public static  final String TV_BASE_URL="https://api.themoviedb.org/3/tv";
+    public  static  final String TV_SEARCH_BASE_URL="https://api.themoviedb.org/3/search/tv";
 }
--- app/src/main/java/com/example/james/moove/HttpService/HttpService.java
+++ app/src/main/java/com/example/james/moove/HttpService/HttpService.java
@@ -52,4 +52,44 @@ public class HttpService {
         return  responseJson;
 
     }
+    public  String getTV(String tv) throws IOException {
+        OkHttpClient client=new OkHttpClient.Builder()
+                .build();
+        HttpUrl.Builder builder=HttpUrl.parse(Constants.TV_SEARCH_BASE_URL).newBuilder();
+        builder.addQueryParameter(Constants.API_KEY_QUERY,Constants.API_KEY);
+        builder.addQueryParameter(Constants.MOVIE_QUERY,tv);
+
+        String url=builder.build().toString();
+
+        Request request=new Request.Builder()
+                .url(url)
+                .build();
+        Call call=client.newCall(request);
+        Response response=call.execute();
+
+        String responseJson=response.body().string();
+        return  responseJson;
+
+
+    }
+    public String getTVTypes(String type) throws IOException{
+        OkHttpClient client=new OkHttpClient.Builder()
+                .build();
+        HttpUrl.Builder builder=HttpUrl.parse(Constants.TV_BASE_URL+type).newBuilder();
+        builder.addQueryParameter(Constants.API_KEY_QUERY,Constants.API_KEY);
+
+
+        String url=builder.build().toString();
+
+        Request request=new Request.Builder()
+                .url(url)
+                .build();
+        Call call=client.newCall(request);
+        Response response=call.execute();
+
+        String responseJson=response.body().string();
+
+        return  responseJson;
+
+    }
 }
